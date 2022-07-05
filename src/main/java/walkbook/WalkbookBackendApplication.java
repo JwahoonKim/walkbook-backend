364de8fdbd7f4be39a6d5f28ebceb6c29370eccd
jwahoonKim <kjhoon0330@snu.ac.kr>
2022-07-06 02:33:53 +0900

    initial commit & 서버 실행 테스트용 컨트롤러 생성

---
+++ src/main/java/walkbook/WalkbookBackendApplication.java
@@ -0,0 +1,13 @@
+package walkbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkbookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalkbookBackendApplication.class, args);
+    }
+
+}
