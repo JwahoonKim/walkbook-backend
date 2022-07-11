cd588290ff670a9e4b907016b8389517d11e1015
jwahoonKim <kjhoon0330@snu.ac.kr>
2022-07-11 21:44:39 +0900

    feat: UserRepository Spring Data Jpa로 변경

--- src/main/java/walkbook/api/repository/UserRepository.java
+++ src/main/java/walkbook/api/repository/UserRepository.java
@@ -1,51 +1,13 @@
 package walkbook.api.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import walkbook.api.domain.model.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.Optional;
 
 @Repository
-public class UserRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public Long save(User user) {
-        em.persist(user);
-        return user.getId();
-    }
-
-    public Optional<User> findByUsername(String username) {
-        return em.createQuery("select u from User u where u.username = :username", User.class)
-                .setParameter("username", username)
-                .getResultStream()
-                .findFirst();
-    }
-
-    public Optional<User> findByNickname(String nickname) {
-        return em.createQuery("select u from User u where u.nickname = :nickname", User.class)
-                .setParameter("nickname", nickname)
-                .getResultStream()
-                .findFirst();
-    }
-
-    public Optional<User> findByUsernameAndPassword(String username, String password) {
-        return em.createQuery("select u from User u where u.username = :username and u.password = :password", User.class)
-                .setParameter("username", username)
-                .setParameter("password", password)
-                .getResultStream()
-                .findFirst();
-    }
-
-    public void remove(User user) {
-        em.remove(user);
-    }
-
-    public User findById(Long id) {
-        return Optional.ofNullable(em.find(User.class, id))
-                .orElseThrow(() -> new RuntimeException("유저 정보를 찾을 수 없습니다. id = " + id));
-    }
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByNickname(String nickname);
 }
--- src/test/java/walkbook/repository/UserRepositoryTest.java
+++ src/test/java/walkbook/repository/UserRepositoryTest.java
@@ -10,6 +10,7 @@ import walkbook.api.repository.UserRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,14 +27,6 @@ class UserRepositoryTest {
     UserRepository userRepository;
 
     @Test
-    public void 유저_조회_실패() {
-        Long invalidId = 1L;
-        String err = assertThrows(RuntimeException.class, () -> userRepository.findById(invalidId))
-                        .getMessage();
-        assertThat(err).isEqualTo("유저 정보를 찾을 수 없습니다. id = " + invalidId);
-    }
-
-    @Test
     public void 회원가입_성공() {
         //given
         User user = User.builder()
@@ -43,11 +36,11 @@ class UserRepositoryTest {
                 .description("안녕하세요.")
                 .build();
         //when
-        Long saveId = userRepository.save(user);
+        User savedUser = userRepository.save(user);
 
         //then
-        User findUser = em.find(User.class, saveId);
-        assertThat(findUser.getId()).isEqualTo(saveId);
+        User findUser = em.find(User.class, savedUser.getId());
+        assertThat(findUser.getId()).isEqualTo(savedUser.getId());
         assertThat(findUser.getUsername()).isEqualTo("user1");
         assertThat(findUser.getPassword()).isEqualTo("1234");
         assertThat(findUser.getNickname()).isEqualTo("jahni");
@@ -139,59 +132,6 @@ class UserRepositoryTest {
     }
 
     @Test
-    public void 아이디와_비밀번호로_조회_성공() {
-        //given
-        User user1 = User.builder()
-                .username("user1")
-                .password("1234")
-                .nickname("jahni")
-                .description("안녕하세요.")
-                .build();
-
-        User user2 = User.builder()
-                .username("user2")
-                .password("1234")
-                .nickname("jahni2")
-                .description("안녕하세요.")
-                .build();
-
-        userRepository.save(user1);
-
-        //when
-        Optional<User> user = userRepository.findByUsernameAndPassword("user1", "1234");
-
-        //then
-        assertThat(user.get().getUsername()).isEqualTo("user1");
-        assertThat(user.get().getPassword()).isEqualTo("1234");
-        assertThat(user.get().getNickname()).isEqualTo("jahni");
-    }
-
-    public void 아이디와_비밀번호로_조회_실패() {
-        //given
-        User user1 = User.builder()
-                .username("user1")
-                .password("1234")
-                .nickname("jahni")
-                .description("안녕하세요.")
-                .build();
-
-        User user2 = User.builder()
-                .username("user2")
-                .password("1234")
-                .nickname("jahni2")
-                .description("안녕하세요.")
-                .build();
-
-        userRepository.save(user1);
-        userRepository.save(user2);
-
-        //when
-        Optional<User> user = userRepository.findByUsernameAndPassword("user1", "123");
-
-        //then
-        assertThat(user.isEmpty()).isEqualTo(true);
-    }
-
     public void 유저_삭제_성공() {
         //given
         User user1 = User.builder()
@@ -211,13 +151,12 @@ class UserRepositoryTest {
         userRepository.save(user1);
         userRepository.save(user2);
 
-        //when
-        userRepository.remove(user2);
-
-        //then
-        String err = assertThrows(RuntimeException.class, () -> userRepository.findById(2L))
-                .getMessage();
-        assertThat(err).isEqualTo("유저 정보를 찾을 수 없습니다. id = " + 2L);
+        //expected
+        List<User> all = userRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+        userRepository.delete(user2);
+        List<User> all2 = userRepository.findAll();
+        assertThat(all2.size()).isEqualTo(1);
     }
 
 }
