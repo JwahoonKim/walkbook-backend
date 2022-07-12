5100c84e9585e5c8261cebbdb37f8b620a70591a
jwahoonKim <kjhoon0330@snu.ac.kr>
2022-07-13 00:39:07 +0900

    refactor: CommentRepository -> Spring Data JPA로 변경

--- src/main/java/walkbook/repository/CommentRepository.java
+++ src/main/java/walkbook/repository/CommentRepository.java
@@ -4,27 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import walkbook.domain.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-//    @PersistenceContext
-//    EntityManager em;
-//
-//    public Long save(Comment comment) {
-//        em.persist(comment);
-//        return comment.getId();
-//    }
-//
-//    public Comment findById(Long id) {
-//        return em.find(Comment.class, id);
-//    }
-//
-//    public Long update(Long id, Comment updateComment) {
-//        Comment comment = em.find(Comment.class, id);
-//        comment.setContent(updateComment.getContent());
-//        return comment.getId();
-//    }
-//
-//    public void remove(Long id) {
-//        Comment comment = em.find(Comment.class, id);
-//        em.remove(comment);
-//    }
 }
